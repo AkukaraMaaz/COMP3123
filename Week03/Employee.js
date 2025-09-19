0ca5916c8a6c94cda1bbc2ad464dfa0c394239df
AkukaraMaaz <mdmaazakukara@gmail.com>
2025-09-18 20:39:03 -0400

    Adding Week03 Folder

---
+++ Week03/Employee.js
@@ -0,0 +1,32 @@
+//TODO - Create Employee Module here and export to use in index.js
+
+let employees = [
+    {id: 1, firstName: "Pritesh", lastName: "Patel", email: "pritesh@gmail.com", Salary:5000},
+    {id: 2, firstName: "Krish", lastName: "Lee", email: "krish@gmail.com", Salary:4000},
+    {id: 3, firstName: "Racks", lastName: "Jacson", email: "racks@gmail.com", Salary:5500},
+    {id: 4, firstName: "Denial", lastName: "Roast", email: "denial@gmail.com", Salary:9000}
+];
+
+//TODO - Function to get all employees
+function getAllEmployees() {
+    return employees;
+}
+
+//TODO - Function to get all employee full names in ascending order
+function getEmployeeNames() {
+    return employees
+        .map(emp => `${emp.firstName} ${emp.lastName}`)
+        .sort();
+}
+
+//TODO - Function to get total salary of all employees
+function getTotalSalary() {
+    return employees.reduce((total, emp) => total + emp.Salary, 0);
+}
+
+// Export functions
+module.exports = {
+    getAllEmployees,
+    getEmployeeNames,
+    getTotalSalary
+};
